49de13211e0354167c24fc8b95c5d42882f48f82
Hivel K <aksoyhvl@gmail.com>
2023-09-06 20:14:58 +0200

    updateng

--- src/test/java/mpack/FirstTest.java
+++ src/test/java/mpack/FirstTest.java
@@ -1,7 +1,6 @@
 package mpack;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
