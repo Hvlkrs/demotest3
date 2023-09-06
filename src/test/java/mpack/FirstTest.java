7bd5d3059410373ee05ea147135c127604a79148
Hivel K <aksoyhvl@gmail.com>
2023-09-06 21:16:10 +0200

    updateng9

--- src/test/java/mpack/FirstTest.java
+++ src/test/java/mpack/FirstTest.java
@@ -1,6 +1,6 @@
 package mpack;
-
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
@@ -13,13 +13,12 @@ public class FirstTest {
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.get("https://www.amazon.de/");
-
         //  driver.findElement(By.id("//*[@id=\"a-autoid-0\"]")).click();
         //  driver.findElement(By.id("//*[@id=\"nav-logo-sprites\"]")).click();
 
-        //driver.close();
+        driver.close();
 
     }
 }
