f31543841b5a8c38e37b74485ab0092df1812f08
Hivel K <aksoyhvl@gmail.com>
2023-09-06 22:10:03 +0200

    updateng13

--- src/test/java/mpack/FirstTest.java
+++ src/test/java/mpack/FirstTest.java
@@ -1,27 +1,26 @@
 package mpack;
 
+import org.junit.Assert;
+import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.testng.Assert;
-import org.testng.annotations.Test;
-
-import java.util.concurrent.TimeUnit;
 
 public class FirstTest {
 
-    WebDriver driver;
 
     @Test
-    public void verifyTitle() {
-//WebDriverManager.chromedriver().setup();
-        ChromeOptions options=new ChromeOptions();
+    public void OpenBrowser()  {
+        WebDriver driver;
+        System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
+        ChromeOptions options = new ChromeOptions();
         options.addArguments("headless");
-        driver=new ChromeDriver(options);
-        driver.get("https://www.browserstack.com/");
-        System.out.println("Title is: " +driver.getTitle());
-        Assert.assertEquals(driver.getTitle(), "App & Browser Testing Made Easy");
-        driver.quit();
+        options.addArguments("disable-gpu");
+        driver = new ChromeDriver(options);
+        driver.get("https://www.google.com");
+        System.out.println("Title of the page is: " + driver.getTitle());
+        Assert.assertTrue("Page title is not correct",driver.getTitle().equals("Google"));
     }
+
 
 }
