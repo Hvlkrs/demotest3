fb85d1c6eb6b8e93202a2bfcc3b4ac228419ace7
Hivel K <aksoyhvl@gmail.com>
2023-09-06 20:52:15 +0200

    updateng7

--- src/test/java/mpack/FirstTest.java
+++ src/test/java/mpack/FirstTest.java
@@ -15,10 +15,11 @@ public class FirstTest {
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
         driver.get("https://www.amazon.de/");
+
         //  driver.findElement(By.id("//*[@id=\"a-autoid-0\"]")).click();
         //  driver.findElement(By.id("//*[@id=\"nav-logo-sprites\"]")).click();
 
-        driver.close();
+        //driver.close();
 
     }
 }
